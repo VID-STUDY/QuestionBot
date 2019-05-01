0135e1aed1cb88a241ef0edd36d328a77a2daa05
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-01 13:57:43 +0500

    Add Vuetify

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="home">
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'home',
-  components: {
+  export default {
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
