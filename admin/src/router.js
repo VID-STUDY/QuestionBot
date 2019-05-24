04292456dcbdabc77374bad4f4b6ea84398c5a49
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-24 20:10:39 +0500

    Create views for adding channels

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import AddChannel from './components/AddChannel'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -27,7 +28,8 @@ export default new Router({
     },
     {
       path: '/addChannel',
-      name: 'newChannel'
+      name: 'newChannel',
+      component: AddChannel
     }
   ]
 })
