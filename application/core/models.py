57bab2f3bf4eaff62774bc0469b1a711dc806c98
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-29 02:42:08 +0500

    Methods for getting tests attributes and creating users

--- application/core/models.py
+++ application/core/models.py
@@ -1,7 +1,6 @@
 from application import db
 from datetime import datetime
-from typing import List
-
+import settings
 
 members = db.Table('channel_members',
                    db.Column('bot_user_id', db.Integer, db.ForeignKey('bot_user.id'), primary_key=True),
@@ -15,6 +14,22 @@ class BotUser(db.Model):
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     answers = db.relationship('Answer', lazy='dynamic', backref=db.backref('user', lazy=True))
+
+    @staticmethod
+    def add_user(user_id, first_name, last_name, username):
+        current_user = BotUser.query.get(user_id)
+        if current_user:
+            return current_user
+        new_user = BotUser(id=user_id, username=username, first_name=first_name, last_name=last_name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    def get_name(self):
+        if self.last_name:
+            return self.first_name + ' ' + self.last_name
+        else:
+            return self.first_name
 
 
 class Channel(db.Model):
@@ -49,6 +64,10 @@ class Channel(db.Model):
     @staticmethod
     def get_by_name(channel_name: str):
         return Channel.query.filter(Channel.channel_name == channel_name).first()
+
+    @staticmethod
+    def get_by_chat_id(channel_chat_id: int):
+        return Channel.query.filter(Channel.chat_id == channel_chat_id)
 
     def is_member_exists(self, bot_user_id: int) -> bool:
         return self.members.filter(members.c.bot_user_id == bot_user_id).count() > 0
@@ -109,9 +128,36 @@ class Test(db.Model):
         db.session.commit()
         return test
 
-    def add_answer(self, user_id: int, answer):
-        if self.answers.filter(Answer.user_id == user_id).count() == 0:
-            self.answers.append(answer)
+    @staticmethod
+    def get_by_id(test_id: int):
+        return Test.query.get(test_id)
+
+    def add_answer(self, answer):
+        self.answers.append(answer)
+        db.session.add(answer)
+        db.session.commit()
+
+    def get_count_user_answers(self, user_id):
+        return self.answers.filter(Answer.user_id == user_id).count()
+
+    def get_right_answer(self) -> Option:
+        return self.options.filter(Option.is_answer == True).first()
+
+    def get_count_of_first_try_answers(self):
+        top_10_points = settings.get_top_10_points()
+        first_try_points = settings.get_first_try_points()
+        return self.answers.query(Answer.points == top_10_points or Answer.points == first_try_points).count()
+
+    def get_top_10_answers_count(self):
+        top_10_points = settings.get_top_10_points()
+        return self.answers.query(Answer.points == top_10_points).count()
+
+    def user_given_right_answer(self, user_id):
+        top_10_points = settings.get_top_10_points()
+        first_try_points = settings.get_first_try_points()
+        return self.answers.query.filter(Answer.user_id == user_id
+                                         and (Answer.points == top_10_points
+                                              or Answer.points == first_try_points)).count() > 0
 
 
 class Answer(db.Model):
