a7e041013425b90b28a7af32fc727982d3298f5d
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-28 22:55:29 +0500

    API endpoint for creating a test

--- application/api/tests.py
+++ application/api/tests.py
@@ -1,5 +1,5 @@
 from application.api import bp
-from application.core.models import Channel
+from application.core.models import Channel, Test
 from application.utils import api as apiutils
 from flask import jsonify, request
 
@@ -12,3 +12,10 @@ def get_channel_tests(channel_name: str):
         return jsonify(error), 404
     return jsonify([test.to_dict() for test in tests]), 200
 
+
+@bp.route('/channels/<string:channel_name>/tests', methods=['POST'])
+def create_test(channel_name: str):
+    data = request.get_json()
+    channel = Channel.get_by_name(channel_name)
+    new_test = Test.create(data, channel)
+    return jsonify(new_test.to_dict()), 200
