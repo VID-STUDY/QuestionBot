e26896ee6b9524851d5f3db12686dc5b8148a69c
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-30 16:35:27 +0500

    Move resources to right folder

--- application/resources/strings.py
+++ application/resources/strings.py
@@ -1,22 +1,10 @@
-"""
-String manager.
-Use get_string method to get string by key
-"""
-
-import os
 import json
+import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-# load strings from json
-strings_json = open(os.path.join(_basedir, 'strings.json'), 'r', encoding='utf8').read()
-_strings = json.loads(strings_json, encoding='utf8')
+_strings = json.loads(open(os.path.join(_basedir, 'strings.json'), 'r').read())
 
 
 def get_string(key: str) -> str:
-    """
-    Get string by key
-    :param key: str
-    :return: str
-    """
     return _strings.get(key, 'no_string')
