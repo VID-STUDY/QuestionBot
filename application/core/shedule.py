6cb82f7e546852957bcfc8e9e88a2675adf4871a
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-31 20:23:24 +0500

    Configure scheduling test and ratings publishing

---
+++ application/core/shedule.py
@@ -0,0 +1,30 @@
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.date import DateTrigger
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+
+from application.bot import publishing
+
+
+_rating_trigger = CronTrigger(day_of_week='sun', hour=12, timezone='Asia/Tashkent')
+_scheduler = BackgroundScheduler()
+_scheduler.add_job(publishing.publish_rating, _rating_trigger)
+_scheduler.start()
+
+
+def add_test_to_publish(test_id: int, date_time: datetime):
+    trigger = DateTrigger(run_date=date_time, timezone='Asia/Tashkent')
+    _scheduler.add_job(publishing.publish_test, trigger, [test_id], id='test_'+str(test_id))
+
+
+def remove_test_from_publishing(test_id: int):
+    _scheduler.remove_job('test_'+str(test_id))
+
+
+def update_test_date_publish(test_id: int, date_time: datetime):
+    remove_test_from_publishing(test_id)
+    add_test_to_publish(test_id, date_time)
+
+
+def stop_all_jobs():
+    _scheduler.remove_all_jobs()
