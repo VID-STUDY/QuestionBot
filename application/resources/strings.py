971b8d4f7886147ec884c4c26ae181e0131a3f4e
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-02 21:40:10 +0500

    Generate message from test

--- application/resources/strings.py
+++ application/resources/strings.py
@@ -8,3 +8,9 @@ _strings = json.loads(open(os.path.join(_basedir, 'strings.json'), 'r').read())
 
 def get_string(key: str) -> str:
     return _strings.get(key, 'no_string')
+
+
+def from_test(test) -> str:
+    test_content = '<b>Вопрос:</b>\n'
+    test_content += test.question
+    return test_content
