0d22bda1b7ef78a2de0d6c677923e8d2b38bed1c
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-02 23:21:54 +0500

    Set channel id for answer

--- application/bot/core.py
+++ application/bot/core.py
@@ -67,7 +67,7 @@ def answers_processor(test_id, option_id, user: User, channel_chat_id, query: Ca
     else:
         answer_points = 0
         message = strings.get_string('answer.wrong_answer')
-    answer = Answer(user_id=user.id, points=answer_points)
+    answer = Answer(user_id=user.id, points=answer_points, channel_id=channel.id)
     test.add_answer(answer)
     try:
         telegram_bot.answer_callback_query(query.id, message, show_alert=True)
