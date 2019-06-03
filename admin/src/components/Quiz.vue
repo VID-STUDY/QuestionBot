1105e9e066074a6c2c2c9115c5e31b61eda21e26
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-03 18:01:48 +0500

    Create quiz component

---
+++ admin/src/components/Quiz.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="quiz-section mb-1">
+        <h4>{{ quiz.startDate }} - {{ quiz.endDate }}</h4>
+        <div class="d-flex justify-end align-center">
+            <v-btn :to="{ name: 'editQuiz', params: {name: name, id: quiz.id} }" flat color='primary'>
+                <v-icon>edit</v-icon>
+            </v-btn>
+            <v-btn flat color='danger'>
+                <v-icon>delete</v-icon>
+            </v-btn>
+        </div>
+        <v-carousel hide-delimiters>
+            <v-carousel-item v-for="test in quiz.tests" :key="test.id">
+                <Test :test='test'></Test>
+            </v-carousel-item>
+        </v-carousel>
+        <v-divide></v-divide>
+    </section>
+</template>
+
+<script>
+import Test from './Test'
+export default {
+    name: 'Quiz',
+    props: ['quiz', 'name'],
+    components: {Test}
+}
+</script>
+
+
