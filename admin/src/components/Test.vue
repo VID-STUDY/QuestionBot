8eeda90b3ee149d83b0ae44b676e90cf6dc338f6
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-03 18:02:40 +0500

    Improve test component, add additional info

--- admin/src/components/Test.vue
+++ admin/src/components/Test.vue
@@ -1,36 +1,40 @@
 <template>
     <v-flex xs12 md4>
         <v-item>
-            <v-card>
-                <v-card-title>
-                    <p class="question mb-0">{{ test.question }}</p>
-                </v-card-title>
-                <v-divider/>
-                <v-card-text>
-                    <v-list subheader>
-                        <v-subheader>Вариаты ответов</v-subheader>
-                        <v-list-tile v-for="option in test.options" :key="option.id" class="option-item" avatar>
-                            <v-list-tile-avatar>
-                                <v-checkbox color="primary" :disabled="!option.isAnswer" @change="checkboxChange" v-model="option.isAnswer"/>
-                            </v-list-tile-avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title class="option-item-title">
-                                    {{ option.value }}
-                                </v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer/>
-                    <v-btn flat color="primary" icon>
-                        <v-icon medium>edit</v-icon>
-                    </v-btn>
-                    <v-btn flat color="red" icon>
-                        <v-icon medium>delete</v-icon>
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
+            <div class="test-item">
+                <v-card>
+                    <v-card-title>
+                        <p class="question mb-0">{{ test.question }}</p>
+                    </v-card-title>
+                    <v-divider/>
+                    <v-card-text>
+                        <p><span class="font-weight-bold">Дата пубикации: </span>{{ test.publishDate }} <span v-if="test.published"><v-icon right color='success'>check</v-icon></span> </p>
+                        <v-list subheader>
+                            <v-subheader>Варианты ответов</v-subheader>
+                            <v-list-tile v-for="option in test.options" :key="option.id" class="option-item" avatar>
+                                <v-list-tile-avatar>
+                                    <v-checkbox color="primary" :disabled="!option.isAnswer" @change="checkboxChange" v-model="option.isAnswer"/>
+                                </v-list-tile-avatar>
+                                <v-list-tile-content>
+                                    <v-list-tile-title class="option-item-title">
+                                        {{ option.value }}
+                                    </v-list-tile-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </v-list>
+                        <p><span class="font-weight-bold">Всего ответов: </span>{{ test.answersCount }}</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer/>
+                        <v-btn flat color="primary" icon>
+                            <v-icon medium>edit</v-icon>
+                        </v-btn>
+                        <v-btn flat color="red" icon>
+                            <v-icon medium>delete</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </div>
         </v-item>
     </v-flex>
 </template>
@@ -44,6 +48,9 @@
 </script>
 
 <style scoped lang="scss">
+    .test-item {
+        height: 260px;
+    }
     p.question {
         font-size: 1rem;
         font-weight: bold;
