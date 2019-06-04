fa6a71255ba09303fdb1dc5370749d8e75c3d771
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-05 00:15:16 +0500

    Channel parent view

---
+++ admin/src/views/Channels/Channel.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <p class="headline">Канал {{ name }}</p>
+        <router-view></router-view>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Channel',
+    props: ['name']
+}
+</script>
+
