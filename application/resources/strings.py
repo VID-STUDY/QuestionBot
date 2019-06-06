b40ea12092adaf463940f939540db877d181fe25
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-06 23:51:09 +0500

    Cancel converting utc to asia - it's useless

--- application/resources/strings.py
+++ application/resources/strings.py
@@ -2,7 +2,6 @@ import json
 import os
 from typing import List, Tuple
 from datetime import datetime
-from application.utils.date import convert_utc_to_asia_tz
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,9 +21,9 @@ def from_test(test) -> str:
 def from_user_points_rating(user_points_list: List[Tuple[str, int]], start_date: datetime, end_date: datetime):
     format_date_str = '%d.%m.%Y'
     rating_content = '<b>Еженедельный рейтинг за {} - {}</b>' \
-        .format(convert_utc_to_asia_tz(start_date).strftime(format_date_str),
-                convert_utc_to_asia_tz(end_date).strftime(format_date_str))
+        .format(start_date.strftime(format_date_str),
+                end_date.strftime(format_date_str))
     rating_content += '\n\n'
     for user_points in user_points_list:
-        rating_content += '{} - {}'.format(user_points[0], user_points[1])
+        rating_content += '{} - {}\n'.format(user_points[0], user_points[1])
     return rating_content
