5e8d4166fdb699cc62a5d8a2070bdaa0ea588fef
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-06 10:53:29 +0500

    Endpoint to save test files

--- application/api/tests.py
+++ application/api/tests.py
@@ -4,27 +4,32 @@ from application.utils import api as apiutils
 from flask import jsonify, request
 
 
-@bp.route('/quizzes/<int:quiz_id>/tests', methods=['GET'])
-def get_channel_tests(quiz_id: str):
-    tests = Quiz.get_tests_by_quiz_id(quiz_id)
-    if tests is None:
-        error = apiutils.error_message(404, 'Викторина не найдена')
-        return jsonify(error), 404
-    return jsonify([test.to_dict() for test in tests]), 200
-
-
-@bp.route('/quizzes/<int:quiz_id>/tests', methods=['POST'])
-def create_test(quiz_id: str):
+@bp.route('/tests', methods=['POST'])
+def create_test():
     data = request.get_json()
-    quiz = Quiz.get_by_id(quiz_id)
-    new_test = Test.create(data, quiz)
+    print(data)
+    new_test = Test.create(data)
     return jsonify(new_test.to_dict()), 200
+
 
 @bp.route('/tests/<int:test_id>', methods=['PUT'])
 def update_test(test_id: int):
     data = request.get_json()
     test = Test.update(test_id, data)
     return jsonify(test.to_dict()), 200
+
+
+@bp.route('/tests/<int:test_id>/file', methods=['POST'])
+def save_test_file(test_id: int):
+    if 'file' not in request.files:
+        error = apiutils.error_message(400, 'Файл не выбран')
+        return jsonify(error)
+    file = request.files['file']
+    if file.filename != '':
+        Test.save_file(test_id, file)
+        return '', 201
+    error = apiutils.error_message(400, 'Файл не выбран')
+    return jsonify(error), 400
 
 
 @bp.route('/tests/<int:test_id>', methods=['GET'])
