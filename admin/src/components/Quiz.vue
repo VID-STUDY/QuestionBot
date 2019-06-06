185ab379f4140e188c0a9c916aa4904d9b368d55
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-06 10:46:32 +0500

    Add buttons events

--- admin/src/components/Quiz.vue
+++ admin/src/components/Quiz.vue
@@ -3,13 +3,10 @@
         <h4>{{ quiz.startDate }} - {{ quiz.endDate }}</h4>
         <div>
             <v-spacer></v-spacer>
-            <v-btn small icon flat color='primary' @click.stop="$emit('openNewTestDialog', quiz.id)">
+            <v-btn small icon flat color='primary' @click.stop="$emit('openNewTestDialog', quiz)">
                 <v-icon>add</v-icon>
             </v-btn>
-            <v-btn small icon :to="{ name: 'editQuiz', params: {name: name, id: quiz.id} }" flat color='primary'>
-                <v-icon>edit</v-icon>
-            </v-btn>
-            <v-btn small flat color='red' icon>
+            <v-btn small flat color='red' icon @click.stop="$emit('openDeleteQuizDialog', quiz)">
                 <v-icon>delete</v-icon>
             </v-btn>
         </div>
@@ -18,7 +15,9 @@
                     <v-layout wrap>
                         <Test v-for="test in quiz.tests"
                             :key="test.id"
-                            :test="test"></Test>
+                            :test="test"
+                            @openDeleteTestDialog="openDeleteTestDialogHandler"
+                            @openEditTestDialog="openEditTestDialogHandler"></Test>
                     </v-layout>
             </v-container>
         </v-item-group>
@@ -32,6 +31,14 @@ export default {
     name: 'Quiz',
     props: ['quiz', 'name'],
     components: {Test},
+    methods: {
+        openDeleteTestDialogHandler(test) {
+            this.$emit('openDeleteTestDialog', test);
+        },
+        openEditTestDialogHandler(test) {
+            this.$emit('openEditTestDialog', test, this.quiz);
+        }
+    }
 }
 </script>
 
