c4b0ad74d5605c8c3f94564480da5c33b1e3237a
Dmitriy <dmitriyl1899@gmail.com>
2019-06-07 18:34:32 +0500

    Redirect to next url

--- application/auth/login.py
+++ application/auth/login.py
@@ -1,8 +1,9 @@
 from . import bp
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_login import login_user, logout_user, login_required, current_user
 from .forms import LoginForm
 from application.core.models import AdminUser
+from werkzeug.urls import url_parse
 
 
 @bp.route('/logout')
@@ -15,10 +16,13 @@ def logout():
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('admin.index'))
+        return redirect(url_for('admin.index_client'))
     form = LoginForm()
     if form.validate_on_submit():
         user = AdminUser.get_by_email(form.email.data)
         login_user(user, remember=False)
-        return redirect(url_for('admin.index'))
-    return render_template('auth/login.html', title="Вход в систему", form=form)
+        next_url = request.args.get('next')
+        if not next_url or url_parse(next_url).netloc != '':
+            return redirect(url_for('admin.index_client'))
+        return redirect(next_url)
+    return render_template('auth/login.html', title="Log in", form=form)
