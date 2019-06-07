9d7813264913b7639f18b89e954009373ff53123
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-08 04:33:37 +0500

    Fix welcome view

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container>
     <v-layout row justify-center>
-      <section class="mt-5">
-        <h1 class="display-1 text-xs-center">Добро пожаловать в административную панель Telegram бота QuestionBot!</h1>
-        <span class="subheading text-xs-center">Данная система позволяет управлять каналами, создавать тесты и опросы, которые публикуются только в то время, которое выберите вы, мониторить пользователей и рейтинги</span>
+      <section class="mt-4">
+        <h1 class="text-xs-center">Добро пожаловать в административную панель Telegram бота QuestionBot!</h1>
+        <div class="mt-3">
+          <p class="subheading text-xs-center">Данная система позволяет управлять каналами, создавать тесты и опросы, которые публикуются только в то время, которое выберите вы, мониторить пользователей и рейтинги</p>
+        </div>
         <v-divider class="my-3"></v-divider>
         <div class="title mb-3 text-xs-center">Для начала работы откройте меню слева в верхнем углу.</div>
       </section>
