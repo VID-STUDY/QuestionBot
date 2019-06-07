0a09b4389d7055e94a2292651b7fbc2e9bc89bbf
Dmitriy <dmitriyl1899@gmail.com>
2019-06-07 23:14:11 +0500

    Justify content

--- admin/src/views/Channels/Channel.vue
+++ admin/src/views/Channels/Channel.vue
@@ -1,6 +1,10 @@
 <template>
     <v-container>
-        <p class="headline">Канал {{ name }}</p>
+        <v-layout wrap row>
+            <v-flex xs12>
+                <p class="headline ml-3">Канал {{ name }}</p>
+            </v-flex>
+        </v-layout>
         <router-view></router-view>
     </v-container>
 </template>
