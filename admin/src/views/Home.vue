a6678d3250acf69035dbd73d3b2b42ea2714ada9
Dmitriy <dmitriyl1899@gmail.com>
2019-06-07 19:25:12 +0500

    Change Home view

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-layout row justify-center>
+      <h3 class="display-3 text-xs-center">Добро пожаловать в административную панель Telegram бота QuestionBot!</h3>
+      <span class="subheading text-xs-center">Данная система позволяет управлять каналами, создавать тесты и опросы, которые публикуются только в то время, которое выберите вы, мониторить пользователей и рейтинги</span>
+      <v-divider class="my-3"></v-divider>
+      <div class="title mb-3">Для начала работы выберите откройте меню слева в верхнем углу.</div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    components: {
-      HelloWorld
-    }
+    name: "Home",
+    title: "QuestionBot Administration"
   }
 </script>
