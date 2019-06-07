2887761453165e1649bc4a770a85d919caa87e80
Dmitriy <dmitriyl1899@gmail.com>
2019-06-07 21:59:14 +0500

    Justify welcome content

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <v-container>
     <v-layout row justify-center>
-      <h3 class="display-3 text-xs-center">Добро пожаловать в административную панель Telegram бота QuestionBot!</h3>
-      <span class="subheading text-xs-center">Данная система позволяет управлять каналами, создавать тесты и опросы, которые публикуются только в то время, которое выберите вы, мониторить пользователей и рейтинги</span>
-      <v-divider class="my-3"></v-divider>
-      <div class="title mb-3">Для начала работы выберите откройте меню слева в верхнем углу.</div>
+      <section class="mt-5">
+        <h1 class="display-1 text-xs-center">Добро пожаловать в административную панель Telegram бота QuestionBot!</h1>
+        <span class="subheading text-xs-center">Данная система позволяет управлять каналами, создавать тесты и опросы, которые публикуются только в то время, которое выберите вы, мониторить пользователей и рейтинги</span>
+        <v-divider class="my-3"></v-divider>
+        <div class="title mb-3 text-xs-center">Для начала работы откройте меню слева в верхнем углу.</div>
+      </section>
     </v-layout>
   </v-container>
 </template>
