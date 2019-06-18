7ecc45cce307e6c605efc91aa2a38d8eac0b7b0d
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-19 02:48:48 +0500

    Form for tests

--- application/admin/forms.py
+++ application/admin/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import StringField, SubmitField, FileField, FieldList, BooleanField, Form, FormField, HiddenField
+from wtforms.validators import DataRequired, ValidationError, Length
 from application.core.services import channels
 from application import telegram_bot
 from telebot.apihelper import ApiException
 import re
+from datetime import datetime
 
 
 class NewChannelForm(FlaskForm):
@@ -55,3 +56,55 @@ class NewQuizForm(FlaskForm):
                 raise ValidationError('Укажите число')
             if int(field.data) < 0:
                 raise ValidationError('Укажите положительное число либо ноль')
+
+
+class OptionForm(Form):
+    value = StringField(validators=[DataRequired('Укажите значение варианта')])
+    is_answer = BooleanField()
+
+
+class TestForm(FlaskForm):
+    question = StringField('Тестовый вопрос',
+                           validators=[DataRequired('Укажите вопрос'),
+                                       Length(max=150, message="Длина вопроса - не более 150 символов.")])
+    publish_date = StringField('День публикации', validators=[DataRequired('Укажите день публикации')])
+    publish_time = StringField('Время публикации', validators=[DataRequired('Укажите время публикации')])
+    file = FileField('Файл')
+    options = FieldList(FormField(OptionForm), min_entries=1)
+    start_date = HiddenField()
+    end_date = HiddenField()
+
+    def init(self):
+        empty_option = OptionForm()
+        empty_option.value.data = 'Вариант ответа'
+        empty_option.is_answer.data = True
+        self.options.append_entry(empty_option)
+
+    def fill_from_object(self, test):
+        self.question.data = test.question
+        self.publish_date.data = test.publish_date.strftime('%d.%m.%Y')
+        self.publish_time.data = test.publish_date.strftime('%H:%M')
+        for option in test.options.all():
+            option_field = OptionForm()
+            option_field.value = option.value
+            option_field.is_answer = option.is_answer
+            self.options.append_entry(option_field)
+        self.start_date.data = test.quiz.start_date.strftime('%d.%m.%Y')
+        self.end_date.data = test.quiz.end_date.strftime('%d.%m.%Y')
+
+    def set_start_end_dates(self, start_date: datetime, end_date: datetime):
+        self.start_date.data = start_date.strftime('%d.%m.%Y')
+        self.end_date.data = end_date.strftime('%d.%m.%Y')
+
+    def validate_publish_date(self, field):
+        if field.data != '':
+            date = datetime.strptime(field.data, '%d.%m.%Y')
+            start_date = datetime.strptime(self.start_date, '%d.%m.%Y')
+            end_date = datetime.strptime(self.end_date, '%d.%m.%Y')
+            if date < start_date or date > end_date:
+                raise ValidationError('День публикации выходит за рамки виктрорины')
+
+    def validate_options(self, field):
+        answers = list(filter(lambda option: option.is_answer, field.entries))
+        if len(answers) == 0:
+            raise ValidationError('Вы не выбрали правильный ответ')
