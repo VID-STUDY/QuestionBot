c91486b3aeaa01a81b13c9e108ae5b82f200d840
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-18 16:57:40 +0500

    Channel CRUD

---
+++ application/admin/channels.py
@@ -0,0 +1,36 @@
+from . import bp
+from application.core.models import Channel
+from application.core.services import channels
+from .forms import NewChannelForm
+from flask import flash, redirect, request, render_template
+from flask_login import login_required
+
+
+@bp.context_processor
+def admin_view_context_processor():
+    return {
+        'channels': Channel.query.all()
+    }
+
+
+@bp.route('/channel/create', methods=['GET', 'POST'])
+@login_required
+def create_channel():
+    form = NewChannelForm()
+    if form.validate_on_submit():
+        chat_id = form.get_channel_chat_id()
+        channel = form.get_channel()
+        channels.create_channel(channel.username, channel.title, chat_id)
+        flash('Канал добавлен', category='success')
+        next_url = request.args.get('next', '/')
+        return redirect(next_url)
+    return render_template('admin/add-channel.html', form=form)
+
+
+@bp.route('/channel/<int:channel_id>/remove')
+@login_required
+def remove_channel(channel_id: int):
+    channels.remove_channel(channel_id)
+    flash('Канал удалён', category='success')
+    next_url = request.args.get('next', '/')
+    return redirect(next_url)
