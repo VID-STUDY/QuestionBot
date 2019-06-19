d1f2fd3559b4f9815c9f30f5b2a850906b8d85bf
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-19 19:36:40 +0500

    Redirect to quizzes after channel created

--- application/admin/channels.py
+++ application/admin/channels.py
@@ -2,7 +2,7 @@ from . import bp
 from application.core.models import Channel
 from application.core.services import channels
 from .forms import NewChannelForm
-from flask import flash, redirect, request, render_template
+from flask import flash, redirect, request, render_template, url_for
 from flask_login import login_required
 
 
@@ -20,10 +20,9 @@ def create_channel():
     if form.validate_on_submit():
         chat_id = form.get_channel_chat_id()
         channel = form.get_channel()
-        channels.create_channel(channel.username, channel.title, chat_id)
+        new_channel = channels.create_channel(channel.username, channel.title, chat_id)
         flash('Канал добавлен', category='success')
-        next_url = request.args.get('next', '/')
-        return redirect(next_url)
+        return redirect(url_for('admin.channel_quizzes', channel_id=new_channel.id))
     return render_template('admin/add-channel.html', form=form)
 
 
