b78af70bac87e0efa2ac3897590984db2b2a525b
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-22 02:23:28 +0500

    Add convert date filter

--- application/utils/filters.py
+++ application/utils/filters.py
@@ -1,6 +1,7 @@
 from application import app
 import os
 from datetime import datetime
+from .date import convert_utc_to_asia_tz
 
 
 @app.template_filter()
@@ -21,3 +22,8 @@ def datetime(value: datetime, format="%d.%m.%Y %H:%M"):
 @app.template_filter()
 def date(value: datetime, format="%d.%m.%Y"):
     return value.strftime(format)
+
+
+@app.template_filter()
+def convert_utc_asia(value: datetime):
+    return convert_utc_to_asia_tz(value)
